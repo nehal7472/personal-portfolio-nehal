f5de3f1634dd7fd56245dcc6f4e7875c582a368d
Ahsanul Haque Nehal <007cryptic@gmail.com>
2025-04-17 14:17:23 +0600

    something changed

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,7 +1,7 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white  text-2xl sm:hidden md:block">Ahsanul Haque</h1>
+      <h1 className="text-white  text-2xl sm:hidden md:block">hsanul Haque</h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
         NEHAL
       </h1>
