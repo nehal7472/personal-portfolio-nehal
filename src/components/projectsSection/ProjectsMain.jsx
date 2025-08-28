4a2f23f85ebfd840e68f20ce142cea5374e0bb31
Ahsanul Haque Nehal <007cryptic@gmail.com>
2025-08-28 21:12:44 +0600

    add some project

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import ProjectsText from "./ProjectsText";
 import SingleProject from "./SingleProject";
@@ -5,6 +6,27 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 
 const projects = [
+  {
+    name: "Book-Store",
+    year: "July2025",
+    align: "left",
+    image: "/images/WebsiteImg09.png",
+    link: "https://nehal-bookstore.vercel.app/",
+  },
+  {
+    name: "E-commerce",
+    year: "July2025",
+    align: "right",
+    image: "/images/WebsiteImg08.png",
+    link: "https://functional-ecommerce-react.vercel.app/",
+  },
+  {
+    name: "Professional Dashboard",
+    year: "July2025",
+    align: "left",
+    image: "/images/WebsiteImg07.png",
+    link: "https://e-commerce-admin-dashboard-topaz.vercel.app",
+  },
   {
     name: "Personal Portfolio",
     year: "Mar2021",
@@ -47,10 +69,56 @@ const projects = [
     image: "/images/WebsiteImg05.png",
     link: "https://nehal7472.github.io/Wall-Clock/",
   },
+  {
+    name: "Simple-ecommerce",
+    year: "August2025",
+    align: "right",
+    image: "/images/WebsiteImg10.png",
+    link: "https://redux-project-chi-vert.vercel.app/",
+  },
+  {
+    name: "Simple-ecommerce",
+    year: "May2025",
+    align: "left",
+    image: "/images/WebsiteImg11.png",
+    link: "https://e-commerce-shoe-nehal.netlify.app/",
+  },
+  {
+    name: "Simple-portfolio",
+    year: "Jan2025",
+    align: "right",
+    image: "/images/WebsiteImg12.png",
+    link: "https://simple-portfolio-nehal.netlify.app/",
+  },
+  {
+    name: "Educational Consultancy",
+    year: "Oct2024",
+    align: "left",
+    image: "/images/WebsiteImg13.png",
+    link: "https://educational-colsutancy.netlify.app/",
+  },
+  {
+    name: "Blog Website",
+    year: "Dec2024",
+    align: "right",
+    image: "/images/WebsiteImg14.png",
+    link: "https://simple-blogs-flax.vercel.app/",
+  },
+  {
+    name: "secure-dice-simulator-game",
+    year: "June2024",
+    align: "left",
+    image: "/images/WebsiteImg15.png",
+    link: "https://github.com/nehal7472/secure-dice-simulator-game",
+  },
 ];
 
 const ProjectsMain = () => {
   const theme = useSelector((state) => state.theme.theme);
+  const [showAll, setShowAll] = useState(false);
+
+  // Show first 4 projects if not expanded
+  const projectsToShow = showAll ? projects : projects.slice(0, 4);
 
   return (
     <div
@@ -67,20 +135,30 @@ const ProjectsMain = () => {
       >
         <ProjectsText />
       </motion.div>
+
       <div className="flex flex-col gap-20 max-w-[900px] mx-auto mt-12">
-        {projects.map((project, index) => {
-          return (
-            <SingleProject
-              key={index}
-              name={project.name}
-              year={project.year}
-              align={project.align}
-              image={project.image}
-              link={project.link}
-            />
-          );
-        })}
+        {projectsToShow.map((project, index) => (
+          <SingleProject
+            key={index}
+            name={project.name}
+            year={project.year}
+            align={project.align}
+            image={project.image}
+            link={project.link}
+          />
+        ))}
       </div>
+
+      {projects.length > 4 && (
+        <div className="text-center mt-8">
+          <button
+            onClick={() => setShowAll(!showAll)}
+            className="px-8 py-3 bg-gradient-to-r from-gray-600 to-gray-800 text-white font-semibold rounded-xl shadow-lg transform transition duration-300 hover:scale-105 hover:from-gray-700 hover:to-gray-900 hover:shadow-2xl border border-gray-500"
+          >
+            {showAll ? "Hide" : "See More"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
